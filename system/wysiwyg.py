339814120711f271c867fe1ab9d58545a1a08d6c
iomatter <30539354+iomatter@users.noreply.github.com>
2017-09-02 15:50:00 -0700

    Update wysiwyg.py

--- system/wysiwyg.py
+++ system/wysiwyg.py
@@ -1,20 +1,23 @@
 import os
 from tkinter import *
 filename = input("filename ")
-file = open(filename, "a")
-root = Tk()
-def addp():
-    text = input("text> ")
-    file.write("print('" + text + "')")
-def addi():
-    modual = input("from ")
-    function = input(" import ")
-    file.write("from " + modual + " import " + function)
-def ececute():
-    filee = input("filename ")
-    os.system(filee)
-Button(root, text='print()', command=addp).pack(side=TOP)
-Button(root, text='from _ import _', command=addi).pack(side=TOP)
-Button(root, text='ececute', command=ececute).pack(side=BOTTOM)
-root.mainloop()
-file.close()
+if filename = "":
+    os.system("system/blank.vbs")
+else:
+	file = open(filename, "a")
+	root = Tk()
+	def addp():
+	    text = input("text> ")
+	    file.write("print('" + text + "')")
+	def addi():
+	    modual = input("from ")
+	    function = input(" import ")
+	    file.write("from " + modual + " import " + function)
+	def ececute():
+	    filee = input("filename ")
+	    os.system(filee)
+	Button(root, text='print()', command=addp).pack(side=TOP)
+	Button(root, text='from _ import _', command=addi).pack(side=TOP)
+	Button(root, text='ececute', command=ececute).pack(side=BOTTOM)
+	root.mainloop()
+	file.close()
