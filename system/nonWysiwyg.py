61c549f8a62afdae6588c98c22569485b0e025e1
iomatter <30539354+iomatter@users.noreply.github.com>
2017-09-02 15:47:35 -0700

    Update nonWysiwyg.py

--- system/nonWysiwyg.py
+++ system/nonWysiwyg.py
@@ -1,22 +1,26 @@
 import tkinter
 from tkinter import *
+import os
 filename = input("filename")
-file = open(filename, "a")
-root = Tk()
-root.title('IOsoft')
-Label(text='Edit').pack(side=TOP,padx=10,pady=10)
+if filename == "":
+    os.system("system/blank.vbs")
+else
+    file = open(filename, "a")
+    root = Tk()
+	root.title('IOsoft')
+	Label(text='Edit').pack(side=TOP,padx=10,pady=10)
 
-entry = Entry(root, width=100)
-entry.pack(side=TOP,padx=10,pady=10)
+	entry = Entry(root, width=100)
+	entry.pack(side=TOP,padx=10,pady=10)
 
-def onok():
-    writedata = entry.get()
-    file.write(writedata)
+	def onok():
+    	writedata = entry.get()
+    	file.write(writedata)
 
-def rekd():
-    root.destroy()
-    file.close()
-Button(root, text='OK', command=onok).pack(side=LEFT)
-Button(root, text='CLOSE', command=rekd).pack(side= RIGHT)
+	def rekd():
+    	root.destroy()
+    	file.close()
+	Button(root, text='OK', command=onok).pack(side=LEFT)
+	Button(root, text='CLOSE', command=rekd).pack(side= RIGHT)
 
-root.mainloop()
+	root.mainloop()
