683ab5fcf46502e5d8feb22ab8419c71d1b13dfa
iomatter <30539354+iomatter@users.noreply.github.com>
2017-09-02 13:54:56 -0700

    Update start.py

--- system/start.py
+++ system/start.py
@@ -1,7 +1,22 @@
-hopefuly = open("boot.log", "a")
-hopefuly.close()
-data = open("boot.log", "r").read()
-if data == "":
-    print("Welcome!")
-else:
-    print("I havent seen you since ", data)
+import tkinter
+from tkinter import *
+filename = input("filename")
+file = open(filename, "a")
+root = Tk()
+root.title('IOsoft')
+Label(text='Edit').pack(side=TOP,padx=10,pady=10)
+
+entry = Entry(root, width=100)
+entry.pack(side=TOP,padx=10,pady=10)
+
+def onok():
+    writedata = entry.get()
+    file.write(writedata)
+
+def rekd():
+    root.destroy()
+    file.close()
+Button(root, text='OK', command=onok).pack(side=LEFT)
+Button(root, text='CLOSE', command=rekd).pack(side= RIGHT)
+
+root.mainloop()
